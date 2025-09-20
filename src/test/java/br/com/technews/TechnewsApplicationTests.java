2c428ff4a810ac26af1c7b7b1fd227d52161aa99
wheslancardoso <wheslancardoso1@gmail.com>
2025-09-20 16:25:15 -0300

    Fix: Adicionar configuração Maven Surefire e simplificar teste principal

--- src/test/java/br/com/technews/TechnewsApplicationTests.java
+++ src/test/java/br/com/technews/TechnewsApplicationTests.java
@@ -2,14 +2,13 @@ package br.com.technews;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
-@ActiveProfiles("test")
+@SpringBootTest
 class TechnewsApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// Este teste verifica se o contexto da aplicação carrega corretamente
 	}
 
 }
