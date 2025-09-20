5ed94b7b0193c17fe0615e448fe6475c118877e7
wheslancardoso <wheslancardoso1@gmail.com>
2025-09-20 16:13:05 -0300

    Fix: Corrigir imports faltantes - Autowired, Modifying e Page

--- src/main/java/br/com/technews/controller/api/NewsletterScheduleApiController.java
+++ src/main/java/br/com/technews/controller/api/NewsletterScheduleApiController.java
@@ -5,6 +5,7 @@ import br.com.technews.entity.Subscriber;
 import br.com.technews.service.NewsletterScheduleService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.format.annotation.DateTimeFormat;
