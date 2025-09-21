9cbe7c4d0e00ba9b1c57b1cc6a9be5b19eee45cb
wheslancardoso <wheslancardoso1@gmail.com>
2025-09-21 11:31:04 -0300

    fix: Especificar classe principal no SimpleIntegrationTest

--- src/test/java/br/com/technews/SimpleIntegrationTest.java
+++ src/test/java/br/com/technews/SimpleIntegrationTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(classes = br.com.technews.TechnewsApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 public class SimpleIntegrationTest {
 
