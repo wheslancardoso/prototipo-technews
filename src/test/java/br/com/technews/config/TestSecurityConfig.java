fc58db8e6813738b9b608327a47906352635cb0c
wheslancardoso <wheslancardoso1@gmail.com>
2025-09-22 22:21:24 -0300

    Fix: Corrigir conflito de configurações de segurança removendo @EnableWebSecurity das configurações de teste

--- src/test/java/br/com/technews/config/TestSecurityConfig.java
+++ src/test/java/br/com/technews/config/TestSecurityConfig.java
@@ -4,11 +4,9 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @TestConfiguration
-@EnableWebSecurity
 public class TestSecurityConfig {
 
     @Bean
